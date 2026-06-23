15a10860b49dfc2bbfbffd7e6d85af19383ecbae
Ekaterina Nikonova <u0441u@gmail.com>
2026-06-23 16:40:25 +0200

    A2 refactor: Javadoc/Benennung, Verhalten unverändert

--- src/main/java/de/projekt/a2/Warenkorb.java
+++ src/main/java/de/projekt/a2/Warenkorb.java
@@ -1,14 +1,20 @@
 package de.projekt.a2;
 
+/**
+ * Warenkorb (TDD). Aktuell noch naiv: es wird nur der zuletzt hinzugefügte Preis
+ * gemerkt. Das reicht für einen Artikel; mehrere Artikel folgen im nächsten Zyklus.
+ */
 public class Warenkorb {
 
-    // vorläufig: nur der zuletzt hinzugefügte Preis
+    /** Vorläufig: nur der zuletzt hinzugefügte Preis. */
     private double letzterPreis = 0.0;
 
+    /** Liefert die aktuelle Gesamtsumme des Warenkorbs. */
     public double getTotal() {
         return letzterPreis;
     }
 
+    /** Fügt einen Artikel hinzu (noch ohne Liste/Mengen). */
     public void addItem(String name, double preis) {
         this.letzterPreis = preis;
     }
