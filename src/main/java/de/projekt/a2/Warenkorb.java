d6594ba9c041b9482b1337adacb3e4e62995c1a2
Ekaterina Nikonova <u0441u@gmail.com>
2026-06-23 16:35:32 +0200

    A2 grün: addItem speichert letzten Preis

--- src/main/java/de/projekt/a2/Warenkorb.java
+++ src/main/java/de/projekt/a2/Warenkorb.java
@@ -2,7 +2,14 @@ package de.projekt.a2;
 
 public class Warenkorb {
 
+    // vorläufig: nur der zuletzt hinzugefügte Preis
+    private double letzterPreis = 0.0;
+
     public double getTotal() {
-        return 0.0;
+        return letzterPreis;
+    }
+
+    public void addItem(String name, double preis) {
+        this.letzterPreis = preis;
     }
 }
